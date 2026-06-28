22c699374b0ce8e84604fcd912f329ae015ee240
tarik56 <tarik56@hotmail.de>
2026-06-28 17:02:47 +0200

    chore: bump gateway container version to 1.1.0

--- src/model/model.py
+++ src/model/model.py
@@ -33,7 +33,7 @@ class Model:
     def _create_gateway(self):
         self._parser.applications.insert(0, dict(
             name="TINYOPS_GATEWAY",
-            image="tarik56/tinyops-gateway:1.0.0",
+            image="tarik56/tinyops-gateway:1.1.0",
             network="tinyops_network",
             replicas=1,
             ports={
